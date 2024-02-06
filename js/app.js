579d5bcd0735b7f08bc7bf05fb5d827c9822352b
francomarmo <francofernandezmarmo@gmail.com>
2024-02-06 13:38:47 -0300

    arregle lo mismo otra vez

--- js/app.js
+++ js/app.js
@@ -23,44 +23,63 @@
 //   window.addEventListener("load", hideMenuOnResize);
 // });
 
+
+
+
+
+
+
+// $(document).ready(function() {
+//   $ ('#autoWidth').lightSlider({
+//       autoWidth:true,
+//       loop:true,
+//       onSliderLoad: function() {
+//           $('#autoWidth').removeClass('cS-hidden');
+//       } 
+//   });  
+// });
+
+
 document.addEventListener("DOMContentLoaded", () => {
   // Seleccion de elementos del DOM
   const toggleButton = document.querySelector(".navbar__toggle-btn");
   const mobileMenu = document.querySelector(".navbar__mobile-menu");
 
-  // event listeners
-  toggleButton.addEventListener("click", () => {
-    mobileMenu.classList.toggle("show");
-  });
+  // Si el menú móvil está oculto ('none' o vacío), lo muestra cambiando a 'flex'.
+  // Si el menú móvil ya está visible ('flex'), lo oculta cambiando a 'none'.
+  const toggleMenu = () => {
+    mobileMenu.style.display =
+      mobileMenu.style.display === "none" || mobileMenu.style.display === ""
+        ? "flex"
+        : "none";
+  };
 
-  // ocultar la lista desplegable al hacer clic en un enlace en el menú móvil
-  mobileMenu.addEventListener("click", () => {
-    mobileMenu.classList.remove("show");
-  });
+  // Ocultar la lista desplegable
+  const hideMenuOnResize = () => {
+    mobileMenu.style.display = "none";
+  };
 
-  // ocultar la lista desplegable al hacer clic en cualquier lugar fuera del menú móvil
-  document.addEventListener("click", (event) => {
-    if (!toggleButton.contains(event.target) && !mobileMenu.contains(event.target)) {
-      mobileMenu.classList.remove("show");
-    }
-  });
+  // Event listeners
+  toggleButton.addEventListener("click", toggleMenu);
+  window.addEventListener("resize", hideMenuOnResize);
+  window.addEventListener("load", hideMenuOnResize);
 
-  // ocultar la lista desplegable en cambios de tamaño de pantalla
-  window.addEventListener("resize", () => {
-    mobileMenu.classList.remove("show");
+  // Cerrar el menú cuando se hace clic en un enlace del menú móvil
+  const mobileLinks = document.querySelectorAll(".nav__link-mobile");
+  mobileLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      mobileMenu.style.display = "none";
+    });
   });
 });
 
-
-
-
-
+// jQuery para el slider
 $(document).ready(function() {
-  $ ('#autoWidth').lightSlider({
-      autoWidth:true,
-      loop:true,
-      onSliderLoad: function() {
-          $('#autoWidth').removeClass('cS-hidden');
-      } 
-  });  
+  $('#autoWidth').lightSlider({
+    autoWidth: true,
+    loop: true,
+    onSliderLoad: function() {
+      $('#autoWidth').removeClass('cS-hidden');
+    }
+  });
 });
