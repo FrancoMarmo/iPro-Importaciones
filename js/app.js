24a4d3985de21e3747ae7c831d2dd559ede50807
francomarmo <francofernandezmarmo@gmail.com>
2024-02-06 13:36:33 -0300

    arregle que en productos no se cierra el hamburguesa

--- js/app.js
+++ js/app.js
@@ -1,27 +1,56 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//   // Seleccion de elementos del DOM
+//   const toggleButton = document.querySelector(".navbar__toggle-btn");
+//   const mobileMenu = document.querySelector(".navbar__mobile-menu");
+
+//   // Si el menú móvil está oculto ('none' o vacío), lo muestra cambiando a 'flex'.
+//   // Si el menú móvil ya está visible ('flex'), lo oculta cambiando a 'none'.
+//   const toggleMenu = () => {
+//     mobileMenu.style.display =
+//       mobileMenu.style.display === "none" || mobileMenu.style.display === ""
+//         ? "flex"
+//         : "none";
+//   };
+
+//   // ocultar la lista desplegable
+//   const hideMenuOnResize = () => {
+//     mobileMenu.style.display = "none";
+//   };
+
+//   // event listeners
+//   toggleButton.addEventListener("click", toggleMenu);
+//   window.addEventListener("resize", hideMenuOnResize);
+//   window.addEventListener("load", hideMenuOnResize);
+// });
+
 document.addEventListener("DOMContentLoaded", () => {
   // Seleccion de elementos del DOM
   const toggleButton = document.querySelector(".navbar__toggle-btn");
   const mobileMenu = document.querySelector(".navbar__mobile-menu");
 
-  // Si el menú móvil está oculto ('none' o vacío), lo muestra cambiando a 'flex'.
-  // Si el menú móvil ya está visible ('flex'), lo oculta cambiando a 'none'.
-  const toggleMenu = () => {
-    mobileMenu.style.display =
-      mobileMenu.style.display === "none" || mobileMenu.style.display === ""
-        ? "flex"
-        : "none";
-  };
-
-  // ocultar la lista desplegable
-  const hideMenuOnResize = () => {
-    mobileMenu.style.display = "none";
-  };
-
   // event listeners
-  toggleButton.addEventListener("click", toggleMenu);
-  window.addEventListener("resize", hideMenuOnResize);
-  window.addEventListener("load", hideMenuOnResize);
+  toggleButton.addEventListener("click", () => {
+    mobileMenu.classList.toggle("show");
+  });
+
+  // ocultar la lista desplegable al hacer clic en un enlace en el menú móvil
+  mobileMenu.addEventListener("click", () => {
+    mobileMenu.classList.remove("show");
+  });
+
+  // ocultar la lista desplegable al hacer clic en cualquier lugar fuera del menú móvil
+  document.addEventListener("click", (event) => {
+    if (!toggleButton.contains(event.target) && !mobileMenu.contains(event.target)) {
+      mobileMenu.classList.remove("show");
+    }
+  });
+
+  // ocultar la lista desplegable en cambios de tamaño de pantalla
+  window.addEventListener("resize", () => {
+    mobileMenu.classList.remove("show");
+  });
 });
+
 
 
 
